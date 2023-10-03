9659562a1611c5ce55646da8012435ace0b6932c
Felipe <feliperubinosan09@gmail.com>
2023-10-03 19:12:41 -0300

    Criado tela inicial

--- Projeto/ProjetoCard/src/screens/Inicial/index.tsx
+++ Projeto/ProjetoCard/src/screens/Inicial/index.tsx
@@ -1,13 +1,31 @@
 import React from "react";
-import { Text } from "react-native";
-import { Container } from "./styles";
+import { Text} from "react-native";
+import { Container, ContainerFoto, ContainerPrincipal, Conteudo, ImagemSecundaria, Imagens, QuartaImagem, TerceiraImagem } from "./styles";
 
 
 
 function Inicial(){
     return (
         <Container>
-            <Text> A  b c </Text>
+            <ContainerPrincipal>
+                <ContainerFoto>
+                    <Imagens
+                        source={require("../../assets/images/Foto1.png")}
+                    />
+                    <ImagemSecundaria
+                        source={require("../../assets/images/Foto2.jpg")}
+                    />
+                    <TerceiraImagem
+                        source={require("../../assets/images/Foto3.jpg")}
+                    />
+                    <QuartaImagem
+                        source={require("../../assets/images/Foto4.png")}
+                    />
+                <Conteudo>
+
+                </Conteudo>
+                </ContainerFoto>
+            </ContainerPrincipal>
         </Container> 
     )
 }
--- Projeto/ProjetoCard/src/theme/index.ts
+++ Projeto/ProjetoCard/src/theme/index.ts
@@ -11,4 +11,4 @@ export default {
       BOLD: 'Roboto_700Bold',
     }
   };
-  
+    
